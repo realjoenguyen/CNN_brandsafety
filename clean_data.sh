138f2670a8884116b9a88d841e9cd3f792363cfb
Tuan Anh <anh_nguyen@knorex.com>
2017-08-02 10:07:53 +0700

    extensive stopwords

--- clean_data.sh
+++ clean_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . ./set_path.sh
-rm -rf ${TRAINDIR} ${TESTDIR} 
-mkdir -pv ${TRAINDIR} ${TESTDIR}
+rm -rf ${TRAINDIR_lemma_extensive} ${TESTDIR_lemma_extensive}
+mkdir -pv ${TRAINDIR_lemma_extensive} ${TESTDIR_lemma_extensive}
 
-python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/train ${TRAINDIR} 
-python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/test ${TESTDIR} 
+python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/train ${TRAINDIR_lemma_extensive} 
+python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/test ${TESTDIR_lemma_extensive} 
