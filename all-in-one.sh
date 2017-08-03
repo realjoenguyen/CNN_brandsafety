be0ae790a3b64d9ac8910c845539e628632920bb
Tuan Anh <anh_nguyen@knorex.com>
2017-08-03 15:41:15 +0700

    up

--- all-in-one.sh
+++ all-in-one.sh
@@ -12,12 +12,12 @@
 #mkdir -pv ${TRAINDIR_REDUCED} ${TESTDIR_REDUCED}
 #python ${PREPROCESS_DIR}/reduce_vocab.py ${TRAINDIR} ${TRAINDIR_REDUCED} ${DATA_OWN}/vocab.pkl 20000
 #python ${PREPROCESS_DIR}/reduce_vocab.py ${TESTDIR} ${TESTDIR_REDUCED} ${DATA_OWN}/vocab.pkl 20000
-#python ${CODE_DIR}/train.py \
-#        --traindir ${TRAINDIR} \
-#        --pretrain True \
-#        --pretrain_data word2vec \
-#        --trainVocab False \
-#        --exp_name newest > train-log.txt
+python ${CODE_DIR}/train.py \
+        --traindir ${TRAINDIR} \
+        --pretrain True \
+        --pretrain_data word2vec \
+        --trainVocab False \
+        --exp_name newest > train-log.txt
 
 python ${CODE_DIR}/eval.py \
         --traindir ${TRAINDIR} \
