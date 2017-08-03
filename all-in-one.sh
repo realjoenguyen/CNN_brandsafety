648473dfb915deb945c818fd3c790b6021e670ca
Tuan Anh <anh_nguyen@knorex.com>
2017-08-03 09:45:56 +0700

    debug

--- all-in-one.sh
+++ all-in-one.sh
@@ -12,13 +12,13 @@ mkdir -pv ${DATA_OWN}
 #mkdir -pv ${TRAINDIR_REDUCED} ${TESTDIR_REDUCED}
 #python ${PREPROCESS_DIR}/reduce_vocab.py ${TRAINDIR} ${TRAINDIR_REDUCED} ${DATA_OWN}/vocab.pkl 20000
 #python ${PREPROCESS_DIR}/reduce_vocab.py ${TESTDIR} ${TESTDIR_REDUCED} ${DATA_OWN}/vocab.pkl 20000
-python ${CODE_DIR}/train.py \
-        --traindir ${TRAINDIR} \
-        --pretrain True \
-        --pretrain_data word2vec \
-        --trainVocab False \
-        --exp_name newest > train-log.txt
+#python ${CODE_DIR}/train.py \
+#        --traindir ${TRAINDIR} \
+#        --pretrain True \
+#        --pretrain_data word2vec \
+#        --trainVocab False \
+#        --exp_name newest > train-log.txt
 
-#python ${CODE_DIR}/eval.py \
-#        --testdir ${TESTDIR} \
-#        --checkpoint_dir='./runs/newest/checkpoints/'
+python ${CODE_DIR}/eval.py \
+        --testdir ${TESTDIR} \
+        --checkpoint_dir='./runs/newest/checkpoints/'
