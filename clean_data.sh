ed6890cc455c5aa3922c766a44b5e656a43a486c
Tuan Anh <anh_nguyen@knorex.com>
2017-08-17 09:45:23 +0700

    clean data

--- clean_data.sh
+++ clean_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . ./set_path.sh
-rm -rf ${TRAINDIR_lemma_extensive} ${TESTDIR_lemma_extensive}
-mkdir -pv ${TRAINDIR_lemma_extensive} ${TESTDIR_lemma_extensive}
+rm -rf ${TRAINDIR_lemma} ${TESTDIR_lemma}
+mkdir -pv ${TRAINDIR_lemma} ${TESTDIR_lemma}
 
-python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/train ${TRAINDIR_lemma_extensive} 
-python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/test ${TESTDIR_lemma_extensive} 
+python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/train ${TRAINDIR_lemma}
+python ${PREPROCESS_DIR}/clean_data.py ${DATA_DIR}/test ${TESTDIR_lemma} 
